a51fb947f8e81119ffe573261317aff0da3d852c
Zazou <aito.kenza@gmail.com>
2024-01-25 14:27:27 +0100

    ajout d'un getMessage

---
+++ utilitaire2Recensement/src/Controller/MessageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+use Illuminate\Http\Request;
+
+class MessageController extends AbstractController
+{
+    #[Route('/message', name: 'app_message')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MessageController.php',
+        ]);
+    }
+    public function getMessages()
+    {
+        $messages = [
+            'MessageController Bonjour',
+        ];
+
+        return $messages;
+    }
+}
